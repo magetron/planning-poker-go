8d55b123ce34025c8923dd39b062bef167e5af9b
Patrick Wu <magetron1@gmail.com>
2019-06-27 13:27:06 +0100

    add minimal working server

--- src/main.go
+++ src/main.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/stretchr/goweb"
+	"github.com/stretchr/goweb/context"
+)
+
+const (
+	Address string = ":8080"
+)
+
+func mapRoutes() {
+	goweb.Map("/", func(c context.Context) error {
+		return goweb.Respond.With(c, 200, []byte("Welcome to Planning Poker backend"))
+	})
+
+}
+
+func main() {
+	mapRoutes()
+
+	log.Print("Staring server ...")
+
+	server := &http.Server{
+		Addr:           Address,
+		Handler:        goweb.DefaultHttpHandler(),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 32,
+	}
+
+	channel := make(chan os.Signal, 1)
+	signal.Notify(channel, os.Interrupt)
+	listener, listenErr := net.Listen("tcp", Address)
+
+	log.Printf("Server running on port %s", Address)
+
+	if listenErr != nil {
+		log.Fatalf("Could not listen %s", listenErr)
+	}
+
+	log.Println("Server routes : ")
+	log.Printf("%s", goweb.DefaultHttpHandler())
+
+	go func() {
+		for _ = range channel {
+			log.Print("Stop Signal Detected. Stopping ...")
+			listener.Close()
+			log.Fatal("Stopped.")
+		}
+	}()
+
+	log.Fatalf("Error in Server: %s", server.Serve(listener))
+
+}
