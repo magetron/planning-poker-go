a0c9de28e7e1a71320bb0e9a1f3af991db1133f5
Patrick Wu <magetron1@gmail.com>
2019-07-08 13:31:03 +0100

    add testing for sprints cycle

--- src/main_test.go
+++ src/main_test.go
@@ -26,8 +26,7 @@ func TestEmptySprint(t *testing.T) {
 
 }
 
-
-func TestNewSprint(t *testing.T) {
+func TestSprintCycle(t *testing.T) {
 
 	codecService := goweb.DefaultHttpHandler().CodecService()
 	handler := handlers.NewHttpHandler(codecService)
@@ -35,6 +34,7 @@ func TestNewSprint(t *testing.T) {
 
 	mapRoutes()
 
+	sprintId := ""
 	goweb.Test(t, goweb.RequestBuilderFunc(func () *http.Request {
 		newReqBody, newReqBodyErr := json.Marshal(map[string]string {
 			"Name": "New Sprint",
@@ -51,5 +51,21 @@ func TestNewSprint(t *testing.T) {
 	}), func(t *testing.T, response *testifyhttp.TestResponseWriter) {
 		assert.Equal(t, http.StatusOK, response.StatusCode, "Status code should be OK for New Sprint.")
 		assert.Equal(t, 25, len(response.Output), "Response Length should be 25 for New Sprint.")
+		sprintId = response.Output[6:15]
 	})
+
+	goweb.Test(t, "GET sprints/" + sprintId, func(t *testing.T, response *testifyhttp.TestResponseWriter) {
+		assert.Equal(t, http.StatusOK, response.StatusCode, "Status code should be OK for Existing Sprint.")
+		assert.Equal(t, `{"d":{"Id":"` + sprintId + `","Name":"New Sprint"},"s":200}`, response.Output, "Response should contain name New Sprint.")
+	})
+
+	goweb.Test(t, "DELETE sprints/" + sprintId, func(t *testing.T, response *testifyhttp.TestResponseWriter) {
+		assert.Equal(t, http.StatusOK, response.StatusCode, "Status code should be OK for Deleting Sprint.")
+	})
+
+	goweb.Test(t, "GET sprints/", func(t *testing.T, response *testifyhttp.TestResponseWriter) {
+		assert.Equal(t, http.StatusOK, response.StatusCode, "Status code should be OK for querying all Sprints.")
+		assert.Equal(t, `{"d":[],"s":200}`, response.Output, "Response will be empty after deleting sprints.")
+	})
+
 }
