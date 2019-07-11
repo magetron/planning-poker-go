a28edf2500c5308e27b8a7b8f0df756d7fc30dd6
elimkwan-at-137591602799 <elimylkwan@gmail.com>
2019-07-11 14:14:00 +0100

    Implement feature that display previous rounds

--- src/ui/src/app/poker/poker-card/poker-card.component.ts
+++ src/ui/src/app/poker/poker-card/poker-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { InternalService } from 'src/app/services/internal.service';
 import { User } from 'src/app/models/user';
+//import { Round } from 'src/app/models/round';
 import { CommsService } from 'src/app/services/comms.service';
 import { Cardify } from '../../models/cardify.component';
 
@@ -16,6 +17,7 @@ export class PokerCardComponent extends Cardify implements OnInit {
   points: number[] = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100, -2]
   @Input() sprint_id: string;
   user: User;
+  //round: Round;
 
   constructor(
     private router: Router,
@@ -33,11 +35,12 @@ export class PokerCardComponent extends Cardify implements OnInit {
     if (!this.user || !this.user.Id) {
       this.router.navigate(["join", this.sprint_id])
     }
+
+    //this.internal.round$.subscribe(msg => this.round = msg);
   }
 
   vote(point: number){
-    this.comms.selectCard(this.sprint_id, this.user.Id, point)
-     .subscribe((response => {
+    this.comms.selectCard(this.sprint_id, this.user.Id, point).subscribe((response => {
         if (response.status === 200) {
           console.log("Selection success");
 
--- src/ui/src/app/services/internal.service.ts
+++ src/ui/src/app/services/internal.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject } from 'rxjs';
 
 import { User} from '../models/user';
 import { Sprint } from '../models/sprint';
+//import { Round } from '../models/round';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,12 @@ export class InternalService {
   private user = new BehaviorSubject<User>({Name: "", Id: "", Vote: -1});
   private sprint = new BehaviorSubject<Sprint>({Name: "", Id: ""});
   private stats = new BehaviorSubject<number[]>([0,0,0]);
+  //private round = new BehaviorSubject<Round>({Name: "default",Id : 0,Avg : 0,Med: 0,Final: 0,Archived : false,CreationTime : 0,});
 
   user$ = this.user.asObservable();
   sprint$ = this.sprint.asObservable();
   stats$ = this.stats.asObservable();
+  //round$ = this.round.asObservable();
 
   constructor() { }
 
@@ -31,5 +34,9 @@ export class InternalService {
   updateStats(stats: number[]) {
     this.stats.next(stats);
   }
+
+  //updateRound(round: Round[]) {
+    //this.round.next(round);
+  //}
 
 }
