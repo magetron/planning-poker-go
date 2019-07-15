e2a80184386d8b50c0bc7522c1e0d7fc805d3dda
Patrick Wu <magetron1@gmail.com>
2019-07-15 11:11:31 +0100

    fix docker script

--- dockerize.sh
+++ dockerize.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
-docker rmi -f planningpoker
+cd src
+echo building...
+make build-linux > /dev/null
+cd ..
+echo done
+echo removing previous docker image...
+docker rmi -f planningpoker > /dev/null
+echo done
+echo building new docker image...
 docker build -t planningpoker .
-docker save -o planningpoker.tar planningpoker
+echo done
+echo saving and uploading docker image...
+docker save -o planningpoker.tar planningpoker > /dev/null
 scp -i planning-poker-admin.pem planningpoker.tar ec2-user@ec2-3-248-4-156.eu-west-1.compute.amazonaws.com:~/
+echo done
+echo executing remote update script...
 ssh -i planning-poker-admin.pem ec2-user@ec2-3-248-4-156.eu-west-1.compute.amazonaws.com "./deploy-docker.sh"
+echo done
  
