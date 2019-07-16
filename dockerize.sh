1b1ac672ee5f066f257991527cb5f46252980182
Patrick Wu <magetron1@gmail.com>
2019-07-16 11:07:38 +0100

    fix deploy script

--- dockerize.sh
+++ dockerize.sh
@@ -12,9 +12,9 @@ docker build -t planningpoker .
 echo done
 echo saving and uploading docker image...
 docker save -o planningpoker.tar planningpoker > /dev/null
-scp -i <path/to/pem/file> planningpoker.tar <username>@ec2-18-200-142-113.eu-west-1.compute.amazonaws.com:~/
+scp -i planningpoker-kp.pem planningpoker.tar ec2-user@ec2-18-200-142-113.eu-west-1.compute.amazonaws.com:~/
 echo done
 echo executing remote update script...
-ssh -i <path/to/pem/file> <username>@ec2-18-200-142-113.eu-west-1.compute.amazonaws.com "./deploy-docker.sh"
+ssh -i planningpoker-kp.pem ec2-user@ec2-18-200-142-113.eu-west-1.compute.amazonaws.com "./deploy-docker.sh"
 echo done
  
