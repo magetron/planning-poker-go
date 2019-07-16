4f7f2b52da464c37e7df94d85c4f34a750c6ea87
Patrick Wu <magetron1@gmail.com>
2019-07-16 09:20:01 +0100

    fix universal docker script

--- dockerize.sh
+++ dockerize.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd src
 echo building...
+make test
 make build-linux > /dev/null
 cd ..
 echo done
@@ -12,9 +13,9 @@ docker build -t planningpoker .
 echo done
 echo saving and uploading docker image...
 docker save -o planningpoker.tar planningpoker > /dev/null
-scp -i planning-poker-admin.pem planningpoker.tar ec2-user@ec2-3-248-4-156.eu-west-1.compute.amazonaws.com:~/
+scp -i <path/to/pem/file> planningpoker.tar <username>@ec2-18-200-142-113.eu-west-1.compute.amazonaws.com:~/
 echo done
 echo executing remote update script...
-ssh -i planning-poker-admin.pem ec2-user@ec2-3-248-4-156.eu-west-1.compute.amazonaws.com "./deploy-docker.sh"
+ssh -i <path/to/pem/file> <username>@ec2-18-200-142-113.eu-west-1.compute.amazonaws.com "./deploy-docker.sh"
 echo done
  
