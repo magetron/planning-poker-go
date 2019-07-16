e7566d59c474508d070589a342205a6143ca0a36
Patrick Wu <magetron1@gmail.com>
2019-07-16 16:46:59 +0100

    fix dns names

--- src/ui/src/app/services/globals-docker.service.ts
+++ src/ui/src/app/services/globals-docker.service.ts
@@ -1,6 +1,6 @@
-export const apiUrl = "http://3.248.4.156:8080";
+export const apiUrl = "http://ec2-18-200-142-113.eu-west-1.compute.amazonaws.com:8080";
 export const baseUrl = location.origin + '/#';
 
-export const voteSocket = "ws://3.248.4.156:8080/userinfo";
+export const voteSocket = "ws://ec2-18-200-142-113.eu-west-1.compute.amazonaws.com:8080/userinfo";
 export const socketRefreshTime = 1000; //how often to update user votes
-export const roundInfoSocket = "ws://3.248.4.156:8080/roundinfo";
+export const roundInfoSocket = "ws://ec2-18-200-142-113.eu-west-1.compute.amazonaws.com:8080/roundinfo";
