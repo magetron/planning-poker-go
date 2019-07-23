495712852c2db9092635ef723beaa2a8343f7852
Patrick Wu <magetron1@gmail.com>
2019-07-23 10:36:52 +0100

    add websocket write in accordance to client

--- src/websocket.go
+++ src/websocket.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 	"encoding/json"
@@ -22,4 +23,47 @@ var wsupgrader = websocket.Upgrader{
 	CheckOrigin: func (r *http.Request) bool {
 		return true
 	},
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeroid)
+	defer func() {
+		ticker.Stop()
+		err := c.Conn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.Send:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, _ = w.Write(message)
+
+			sendLen := len(c.Send)
+
+			for i := 0; i < sendLen; i++ {
+				w.Write(<-c.Send)
+			}
+
+			if err := w.Close(); err != nil {
+				log.Fatal(err)
+			}
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 }
