267358736146bec2537b2128b93ec1d071faa5fa
Patrick Wu <magetron1@gmail.com>
2019-07-23 16:47:23 +0100

    add hub controller

--- src/hub_controller.go
+++ src/hub_controller.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/stretchr/goweb"
+	"github.com/stretchr/goweb/context"
+)
+
+type HubController struct {
+	Hubs []*ConnHub
+}
+
+func (hc *HubController) handleHubs (ctx context.Context) error {
+	sprintId := ctx.PathValue("sprintId");
+	for _, hub := range hc.Hubs {
+		if hub.Id == sprintId {
+			wsHandler(sprintId, hub, ctx)
+			return goweb.Respond.WithOK(ctx)
+		}
+	}
+	hub := NewConnHub(sprintId)
+	go hub.Run()
+	hc.Hubs = append(hc.Hubs, hub)
+	wsHandler(sprintId, hub, ctx)
+	return goweb.Respond.WithOK(ctx)
+}
