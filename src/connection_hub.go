42efaf89ba15f6c67295c679f5adab3ef168f4e1
Patrick Wu <magetron1@gmail.com>
2019-07-23 10:28:09 +0100

    add connection hub for ws client

---
+++ src/connection_hub.go
@@ -0,0 +1,48 @@
+package main
+
+import "github.com/gorilla/websocket"
+
+type Client struct {
+	Name string
+	Hub *ConnHub
+	Conn *websocket.Conn
+	Send chan []byte
+}
+
+type ConnHub struct {
+	Clients map[*Client]string
+	Broadcast chan []byte
+	Register chan *Client
+	Unregister chan*Client
+}
+
+func NewConnHub() *ConnHub {
+	return &ConnHub {
+		Clients: make(map[*Client]string),
+		Broadcast: make(chan []byte),
+		Register: make(chan *Client),
+		Unregister: make(chan *Client),
+	}
+}
+
+func (hub *ConnHub) Run() {
+	for {
+		select {
+			case client := <- hub.Register: hub.Clients[client] = client.Name
+			case client := <- hub.Unregister:
+				if _, ok := hub.Clients[client]; ok {
+					delete(hub.Clients, client)
+					close(client.Send)
+				}
+			case message := <- hub.Broadcast:
+				for client := range hub.Clients {
+					select {
+						case client.Send <- message:
+						default:
+							close(client.Send)
+							delete(hub.Clients, client)
+					}
+				}
+		}
+	}
+}
