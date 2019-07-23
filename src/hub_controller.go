20c7d07d9bac9242b0c8705ec7eaca10abc92a79
Patrick Wu <magetron1@gmail.com>
2019-07-23 16:58:00 +0100

    fix error on hub controller

--- src/hub_controller.go
+++ src/hub_controller.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/stretchr/goweb"
 	"github.com/stretchr/goweb/context"
 )
 
@@ -13,13 +12,13 @@ func (hc *HubController) handleHubs (ctx context.Context) error {
 	sprintId := ctx.PathValue("sprintId");
 	for _, hub := range hc.Hubs {
 		if hub.Id == sprintId {
-			wsHandler(sprintId, hub, ctx)
-			return goweb.Respond.WithOK(ctx)
+			err := wsHandler(sprintId, hub, ctx)
+			return err
 		}
 	}
 	hub := NewConnHub(sprintId)
 	go hub.Run()
 	hc.Hubs = append(hc.Hubs, hub)
-	wsHandler(sprintId, hub, ctx)
-	return goweb.Respond.WithOK(ctx)
+	err := wsHandler(sprintId, hub, ctx)
+	return err
 }
