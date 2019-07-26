ba538876417e2c313884cdb600b8b0d51e82ae86
Przemek Pikuła <przemek.pikula@yahoo.com>
2019-07-26 16:15:09 +0100

    Add a docker URL for the new socket

--- src/ui/src/app/services/globals-docker.service.ts
+++ src/ui/src/app/services/globals-docker.service.ts
@@ -1,6 +1,7 @@
 export const apiUrl = "http://planning-poker-nlb-febe808a469ef781.elb.eu-west-1.amazonaws.com:8080";
 export const baseUrl = location.origin + '/#';
 
-export const voteSocket = "ws://planning-poker-nlb-febe808a469ef781.elb.eu-west-1.amazonaws.com:8080/userinfo";
+//export const voteSocket = "ws://planning-poker-nlb-febe808a469ef781.elb.eu-west-1.amazonaws.com:8080/userinfo";
 export const socketRefreshTime = 1000; //how often to update user votes
-export const roundInfoSocket = "ws://planning-poker-nlb-febe808a469ef781.elb.eu-west-1.amazonaws.com:8080/roundinfo";
+//export const roundInfoSocket = "ws://planning-poker-nlb-febe808a469ef781.elb.eu-west-1.amazonaws.com:8080/roundinfo";
+export const infoSocket = "ws://planning-poker-nlb-febe808a469ef781.elb.eu-west-1.amazonaws.com:8080/info/";
