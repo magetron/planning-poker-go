2635e34e978412916036548ffc1b375205465276
Patrick Wu <magetron1@gmail.com>
2019-07-29 09:32:51 +0100

    fix global service addresses

--- src/ui/src/app/services/globals-docker.service.ts
+++ src/ui/src/app/services/globals-docker.service.ts
@@ -1,6 +1,5 @@
-export const apiUrl = "http://ec2-18-200-142-113.eu-west-1.compute.amazonaws.com:8080";
+export const apiUrl = "http://planning-poker-nlb-febe808a469ef781.elb.eu-west-1.amazonaws.com:8080";
 export const baseUrl = location.origin + '/#';
 
-export const voteSocket = "ws://ec2-18-200-142-113.eu-west-1.compute.amazonaws.com:8080/userinfo";
 export const socketRefreshTime = 1000; //how often to update user votes
-export const roundInfoSocket = "ws://ec2-18-200-142-113.eu-west-1.compute.amazonaws.com:8080/roundinfo";
+export const infoSocket = "ws://planning-poker-nlb-febe808a469ef781.elb.eu-west-1.amazonaws.com:8080/info/";
