866b1b86058a9e2d37a724abdc636207c702ce1f
Patrick Wu <magetron1@gmail.com>
2019-07-29 15:42:38 +0100

    add ws test update

--- src/websocket_test.go
+++ src/websocket_test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/goweb"
 	"github.com/stretchr/goweb/handlers"
+	"github.com/stretchr/testify/assert"
+	testifyhttp "github.com/stretchr/testify/http"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -11,14 +16,14 @@ import (
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
+	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
 
-func TestHub (t *testing.T) {
+func TestUpdate(t *testing.T) {
 	codecService := goweb.DefaultHttpHandler().CodecService()
 	handler := handlers.NewHttpHandler(codecService)
 	goweb.SetDefaultHttpHandler(handler)
@@ -27,8 +32,60 @@ func TestHub (t *testing.T) {
 	defer server.Close()
 
 	sprintId := ""
+	goweb.Test(t, goweb.RequestBuilderFunc(func() *http.Request {
+		newReqBody, newReqBodyErr := json.Marshal(map[string]string{
+			"Name": "New Sprint",
+		})
+		if newReqBodyErr != nil {
+			log.Fatal(newReqBodyErr)
+		}
+		newReq, newErr := http.NewRequest("POST", "sprints/", bytes.NewBuffer(newReqBody))
+		if newErr != nil {
+			log.Fatal(newErr)
+		}
+		newReq.Header.Set("Content-Type", "application/json")
+		return newReq
+	}), func(t *testing.T, response *testifyhttp.TestResponseWriter) {
+		sprintId = response.Output[6:15]
+	})
 
-	url := "ws" + strings.Trim(server.URL, "http") + "info/" + sprintId
+	userId1 := ""
+	userId2 := ""
+	goweb.Test(t, goweb.RequestBuilderFunc(func() *http.Request {
+		newReqBody, newReqBodyErr := json.Marshal(map[string]string{
+			"Name": "New User 1",
+		})
+		if newReqBodyErr != nil {
+			log.Fatal(newReqBodyErr)
+		}
+		newReq, newErr := http.NewRequest("POST", "sprints/"+sprintId+"/users/", bytes.NewBuffer(newReqBody))
+		if newErr != nil {
+			log.Fatal(newErr)
+		}
+		newReq.Header.Set("Content-Type", "application/json")
+		return newReq
+	}), func(t *testing.T, response *testifyhttp.TestResponseWriter) {
+		userId1 = response.Output[12:48]
+	})
+
+	goweb.Test(t, goweb.RequestBuilderFunc(func() *http.Request {
+		newReqBody, newReqBodyErr := json.Marshal(map[string]string{
+			"Name": "New User 2",
+		})
+		if newReqBodyErr != nil {
+			log.Fatal(newReqBodyErr)
+		}
+		newReq, newErr := http.NewRequest("POST", "sprints/"+sprintId+"/users/", bytes.NewBuffer(newReqBody))
+		if newErr != nil {
+			log.Fatal(newErr)
+		}
+		newReq.Header.Set("Content-Type", "application/json")
+		return newReq
+	}), func(t *testing.T, response *testifyhttp.TestResponseWriter) {
+		userId2 = response.Output[12:48]
+	})
+
+	url := "ws" + strings.Trim(server.URL, "http") + "/info/" + sprintId
 
 	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
@@ -36,7 +93,10 @@ func TestHub (t *testing.T) {
 	}
 	defer ws.Close()
 
-
+	if err := ws.WriteMessage(websocket.TextMessage, []byte("update")); err != nil {
+		t.Fatalf("%v", err)
+	}
+	_, p, err := ws.ReadMessage()
+	assert.Equal(t, `[[{"Id":"`+userId1+`","Name":"New User 1","Vote":-1,"Admin":true},{"Id":"`+userId2+`","Name":"New User 2","Vote":-1,"Admin":false}]]`, string(p), "Websocket should be Two Users object.")
 
 }
-
