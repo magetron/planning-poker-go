685fc85caf1eef6265e5bfc5b50b2a6d5421a9c8
Patrick Wu <magetron1@gmail.com>
2019-08-01 13:30:20 +0100

    refactor gc

--- src/garbage_collector.go
+++ src/garbage_collector.go
@@ -10,13 +10,7 @@ func garbageCollector(ctx context.Context) error {
 	log.Print("Collecting Garbage...")
 	for _, s := range sc.Sprints {
 		if time.Now().Sub(s.CreationTime).Hours() > 12 {
-			for irs, rs := range rc.AllRounds {
-				if rs.SprintId == s.Id {
-					rc.AllRounds[len(rc.AllRounds)-1], rc.AllRounds[irs] = rc.AllRounds[irs], rc.AllRounds[len(rc.AllRounds)-1]
-					rc.AllRounds = rc.AllRounds[:len(rc.AllRounds)-1]
-					break
-				}
-			}
+			delete(rc.AllRounds, s.Id)
 			delete(us.AllUsers, s.Id)
 			delete(sc.Sprints, s.Id)
 			continue
