5ff35442c4a645dd89cdc09ea9b5353bb3707523
Przemek Pikuła <przemek.pikula@yahoo.com>
2019-08-02 14:12:36 +0100

    Run tests in order to stop random failures

--- src/ui/karma-headless.conf.js
+++ src/ui/karma-headless.conf.js
@@ -13,7 +13,10 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        random: false
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/ui'),
@@ -27,6 +30,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: true,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    
   });
 };
