00876669aecaa4be7d97dfb04fbda91aea2e64c0
Patrick Wu <magetron1@gmail.com>
2019-08-02 14:42:21 +0100

    modify integration env addr

--- src/ui/src/environments/environment.int.ts
+++ src/ui/src/environments/environment.int.ts
@@ -4,6 +4,6 @@ export const environment = {
 
   baseUrl: location.origin + '/#',
 
-  apiUrl: "http://ec2-18-200-142-113.eu-west-1.amazonaws.com:8080",
-  infoSocket : "ws://ec2-18-200-142-113.eu-west-1.amazonaws.com:8080/info/"
+  apiUrl: "http://planning-poker-nlb-ec2-e86c54390baa9c80.elb.eu-west-1.amazonaws.com:8080",
+  infoSocket : "ws://planning-poker-nlb-ec2-e86c54390baa9c80.elb.eu-west-1.amazonaws.com:8080/info/"
 };
