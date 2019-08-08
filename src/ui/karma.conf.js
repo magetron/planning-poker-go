ce9b8735ef75485644c8c407d48e3d51fe5a44d2
Przemek Pikuła <przemek.pikula@yahoo.com>
2019-08-08 15:58:19 +0100

    Fix Karma 2

--- src/ui/karma.conf.js
+++ src/ui/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml', 'dots'], 
     port: 9876, 
     colors: true, 
-    logLevel: config.LOG_DEBUG, 
+    logLevel: config.LOG_INFO, 
     autoWatch: true, 
     browsers: ['Chrome'], 
     singleRun: false, 
