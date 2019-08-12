19e2b6eb3ec9cc02d4610bfb82abc3d1cca6f0d2
Patrick Wu <magetron1@gmail.com>
2019-08-12 14:26:01 +0100

    fix recommend bar

--- src/ui/src/app/poker/poker-control/poker-control.component.ts
+++ src/ui/src/app/poker/poker-control/poker-control.component.ts
@@ -172,4 +172,19 @@ export class PokerControlComponent implements OnInit {
 
     return min + ":" + sec
   }
+
+  copylink(link: string) {
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = link;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
 }
