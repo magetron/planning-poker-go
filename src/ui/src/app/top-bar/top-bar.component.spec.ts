b067a37ff60019ec9e826bd514c386389642b851
Patrick Wu <magetron1@gmail.com>
2019-08-12 10:31:08 +0100

    fix top bar

--- src/ui/src/app/top-bar/top-bar.component.spec.ts
+++ src/ui/src/app/top-bar/top-bar.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatCardModule, MatFormFieldModule, MatIconModule, MatListModule, MatTableModule, MatButtonModule, MatInputModule,  MatToolbarModule } from '@angular/material';
+import { MatCardModule, MatMenuModule, MatFormFieldModule, MatIconModule, MatListModule, MatTableModule, MatButtonModule, MatInputModule,  MatToolbarModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
