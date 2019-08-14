b2fe679c73610ebad8d91e310ac1dad7a8ca2d10
Przemek Pikuła <przemek.pikula@yahoo.com>
2019-08-14 12:26:56 +0100

    Rework timers to hide hours if less than an hour passed use more local variables

--- src/ui/src/app/poker/elapsed-timer/elapsed-timer.component.ts
+++ src/ui/src/app/poker/elapsed-timer/elapsed-timer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Timer } from 'easytimer.js';
 
 import { Round } from '../../models/round';
@@ -9,50 +10,47 @@ import { InternalService } from 'src/app/services/internal.service';
   templateUrl: './elapsed-timer.component.html',
   styleUrls: ['./elapsed-timer.component.css']
 })
+
 export class ElapsedTimerComponent implements OnInit {
 
-  timePassed = 0;
-  rounds: Round[];
-  referenceTime: number;
-  gettingRefTime = true;
+  gettingRefTime: boolean = true
   timer: Timer = new Timer();
+  displayedTime = "0:00"
+  subscriber: Subscription
 
   constructor(
     private internal: InternalService,
   ) { }
 
   ngOnInit() {
-
-    this.internal.rounds$.subscribe(msg => {
-      this.rounds = msg
-      if (this.rounds && this.rounds[0].CreationTime && this.gettingRefTime) {
-        this.referenceTime = this.rounds[0].CreationTime
-        this.getRefTime();
-      } else {
-        this.referenceTime = new Date().getTime()
+    this.subscriber = this.internal.rounds$.subscribe(rounds => {
+      if (rounds && rounds[0].CreationTime && this.gettingRefTime) {
+        this.getRefTime(rounds[0].CreationTime);
+        this.gettingRefTime = false //TODO: substitute with line below ()
+        //this.subscriber.unsubscribe()
       }
     })
-
   }
 
-
-  getRefTime(){
-    this.gettingRefTime = false;
-    this.timePassed = (new Date().getTime()/1000 - this.referenceTime)
-    this.startTimer();
+  getRefTime(referenceTime: number): void {
+    let timePassed = (new Date().getTime()/1000 - referenceTime)
+    this.startTimer(timePassed);
   }
 
-  startTimer(){
+  startTimer(timePassed: number): void {
     let self = this;
-    this.timer.start({precision: 'seconds', startValues: {seconds: this.timePassed} });
-    this.timer.addEventListener('secondsUpdated', function (e){
-      let exist = document.getElementById("elapsedTime")
-      if (exist){
-        exist.innerText = self.timer.getTimeValues().toString().slice(3)
-      }
+    this.timer.start({precision: 'seconds', startValues: {seconds: timePassed} });
+    this.timer.addEventListener('secondsUpdated', function (e) {
+      self.displayedTime = self.timerToString()
     });
   }
 
-  
-
+  timerToString(): string {
+  let hh = this.timer.getTimeValues().hours
+    if (hh) {
+      return `${hh}:${this.timer.getTimeValues().minutes.toString()}:${this.timer.getTimeValues().toString().slice(6)}`
+    } else {
+      return `${this.timer.getTimeValues().minutes.toString()}:${this.timer.getTimeValues().toString().slice(6)}`
+    }
+  }
 }
