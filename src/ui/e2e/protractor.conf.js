16da7aaef57a0e156b65f7c2be94eebec730a686
Patrick Wu <magetron1@gmail.com>
2019-08-21 09:52:32 +0100

    add headless protractor

--- src/ui/e2e/protractor.conf.js
+++ src/ui/e2e/protractor.conf.js
@@ -13,7 +13,10 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+	chromeOptions: {
+		args: ["--headless", "--disable-gpu", "--no-sandbox", "--remote-debugging-port=12345" ]
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/#/',
